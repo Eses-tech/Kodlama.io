e8998d40ae02e4871192fb252e374a8d669d9c69
Eser <Eser@LAPTOP-EVSNA4JH>
2021-02-23 05:13:25 +0300

    23.02.2021

---
+++ GameProject/Concrete/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using GameProject.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Concrete
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(IUser user)
+        {
+            if (user.FirstName == "AYDIN" && user.LastName == "TUZ" && user.IdentityNumber == 11111111111 && user.BirthYear == 1986)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
